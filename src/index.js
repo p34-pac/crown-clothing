f1f73c43626df0552290b0e80baac06826f9620b
p34-pac <peterpaultolulope@gmail.com>
2023-04-07 09:26:39 +0100

    sum

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const app = ReactDOM.createRoot(document.getElementById('root'));
+app.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
