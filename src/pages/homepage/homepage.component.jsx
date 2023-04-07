b10f44139033429574e131a3fa46e3948e69c5a9
p34-pac <peterpaultolulope@gmail.com>
2023-04-07 10:46:38 +0100

    broken home page components and finished styling the menu item

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import './homepage.styles.scss'
+import Directory from "../../components/menu-item/menu-item/directory/directory.components";
+
+const HomePage = ()=>(
+        <div className="homepage">
+            <Directory />
+        </div>
+)
+
+export default HomePage;
